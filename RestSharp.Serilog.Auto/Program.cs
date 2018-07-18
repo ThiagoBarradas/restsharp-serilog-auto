0e98c81503de6744f1af097198fed7670a47142f
Thiago Barradas <thiagobrowskt@gmail.com>
2018-07-18 20:38:40 -0300

    Add project files.

---
+++ RestSharp.Serilog.Auto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestSharp.Serilog.Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
